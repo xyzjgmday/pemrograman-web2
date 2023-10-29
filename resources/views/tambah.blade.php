4b98e626d0f52922c4844875c37f209169c1c489
xyzjgmday <daysubhiz@gmail.com>
2023-10-29 12:48:45 +0700

    tugas-5

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -4,21 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Belajar Laravel</title>
+    <!-- Tambahkan pustaka Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
-    <h3>Data Pegawai</h3>
-    <a href="/pegawai">Kembali</a>
-    <br/>
-    <br/>
+    <div class="container">
+        <h3>Data Pegawai</h3>
+        <a href="/pegawai" class="btn btn-primary">Kembali</a>
+        <br/>
+        <br/>
 
-    <form action="/pegawai/store" method="post">
-        @csrf <!-- Gunakan @csrf untuk menghasilkan token CSRF -->
-        Nama <input type="text" name="nama" required="required"><br/>
-        Jabatan <input type="text" name="posisi" required="required"><br/> <!-- Ganti nama kolom menjadi "posisi" -->
-        Umur <input type="number" name="umur" required="required"><br/>
-        Alamat <textarea name="alamat" required="required"></textarea><br/>
-        <input type="submit" value="Simpan Data">
-    </form>
+        <form action="/pegawai/store" method="post">
+            @csrf
+            <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" id="nama" name="nama" required="required">
+            </div>
+            <div class="form-group">
+                <label for="posisi">Jabatan</label>
+                <input type="text" class="form-control" id="posisi" name="posisi" required="required">
+            </div>
+            <div class="form-group">
+                <label for="umur">Umur</label>
+                <input type="number" class="form-control" id="umur" name="umur" required="required">
+            </div>
+            <div class="form-group">
+                <label for="alamat">Alamat</label>
+                <textarea class="form-control" id="alamat" name="alamat" required="required"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Simpan Data</button>
+        </form>
+    </div>
 
+    <!-- Tambahkan pustaka Bootstrap JavaScript dan jQuery (jika diperlukan) -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
