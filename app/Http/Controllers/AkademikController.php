9dd056d83aee753e1214dfea5887fcddcd15bc9a
xyzjgmday <daysubhiz@gmail.com>
2023-12-13 21:11:48 +0700

    tugas-2

---
+++ app/Http/Controllers/AkademikController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AkademikController extends Controller
+{
+    public function index() {
+        return "Latihan membuat method index, pada controller Akademik";
+    }
+}
---
+++ app/Http/Controllers/DosenController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DosenController extends Controller
+{
+    public function index() {
+        return "Latihan membuat method index, pada controller Dosen";
+    }
+}
